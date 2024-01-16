a9eaaeaf196d45d1fa541cd1c19c7d7f80debba6
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-01-15 17:50:34 -0800

    Explore Page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg';
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg';
 
 const Explore = () => {
   return (
-    <div>
-      <h1>Explore</h1>
+    <div className='explore'>
+      <header>
+        <p className='pageHeader'>Explore</p>
+      </header>
+
+      <main>
+        {/* slider */}
+
+        <p className='exploreCategoryHeading'>Categories</p>
+        <div className='exploreCategories'>
+          <Link to={'/category/rent'}>
+            <img
+              src={rentCategoryImage}
+              alt='Rent'
+              className='exploreCategoryImg'
+            />
+            <p className='exploreCategoryName'>Places for Rent</p>
+          </Link>
+          <Link to={'/category/sell'}>
+            <img
+              src={sellCategoryImage}
+              alt='Rent'
+              className='exploreCategoryImg'
+            />
+            <p className='exploreCategoryName'>Places for Sale</p>
+          </Link>
+        </div>
+      </main>
     </div>
   );
 };
