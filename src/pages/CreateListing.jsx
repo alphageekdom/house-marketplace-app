cd00e03175f9eb7e70abbcefa6ac7fe673a67af1
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-01-16 12:39:25 -0800

    create single listing page and fetch data

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -114,7 +114,6 @@ const CreateListing = () => {
     } else {
       geolocation.lat = latitude;
       geolocation.lng = longitude;
-      location = address;
     }
 
     // Store image in firebase
@@ -171,9 +170,9 @@ const CreateListing = () => {
       timestamp: serverTimestamp(),
     };
 
+    formDataCopy.location = address;
     delete formDataCopy.images;
     delete formDataCopy.address;
-    location && (formDataCopy.location = location);
     !formDataCopy.offer && delete formDataCopy.discountedPrice;
 
     const docRef = await addDoc(collection(db, 'listings'), formDataCopy);
